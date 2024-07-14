39389eaf74b3095aaff04542099516ffcd447257
Narunart Tuntue <narunart.t@arise.tech>
2024-07-14 19:17:41 +0700

    [Sky] add test e2e patch status and patch title

--- e2e/api/todo.spec.ts
+++ e2e/api/todo.spec.ts
@@ -89,3 +89,69 @@ test("should response todo when delete api/v1/todos/:id", async ({
   const deleteResult = await deleteResponse.json();
   expect(deleteResult).toEqual("succes");
 });
+
+test("should response todo when PATCH api/v1/todos/:id/actions/status", async ({
+  request,
+}) => {
+  const postData = {
+    title: "Test Todo",
+    status: "Active",
+  };
+  const repsPost = await request.post("api/v1/todos", { data: postData });
+  const postJson = await repsPost.json();
+
+  const patchStatus = {
+    status: "inactive",
+  };
+  const reps = await request.patch(
+    "api/v1/todos/" + postJson.id + "/actions/status",
+    {
+      data: patchStatus,
+    }
+  );
+
+  expect(reps.ok()).toBeTruthy();
+  expect(await reps.json()).toEqual(
+    expect.objectContaining({
+      id: expect.any(Number),
+      title: expect.any(String),
+      status: patchStatus.status,
+    })
+  );
+
+  const deleteResponse = await request.delete(`/api/v1/todos/${postJson.id}`);
+  expect(deleteResponse.ok()).toBeTruthy();
+});
+
+test("should response todo when PATCH api/v1/todos/:id/actions/title", async ({
+  request,
+}) => {
+  const postData = {
+    title: "Test Todo",
+    status: "Active",
+  };
+  const repsPost = await request.post("api/v1/todos", { data: postData });
+  const postJson = await repsPost.json();
+
+  const patchTitle = {
+    title: "todo 2",
+  };
+  const reps = await request.patch(
+    "api/v1/todos/" + postJson.id + "/actions/title",
+    {
+      data: patchTitle,
+    }
+  );
+
+  expect(reps.ok()).toBeTruthy();
+  expect(await reps.json()).toEqual(
+    expect.objectContaining({
+      id: expect.any(Number),
+      title: patchTitle.title,
+      status: expect.any(String),
+    })
+  );
+
+  const deleteResponse = await request.delete(`/api/v1/todos/${postJson.id}`);
+  expect(deleteResponse.ok()).toBeTruthy();
+});
