a984a851cf9e358e7c96ff84ef380a3f946ecc46
Narunart Tuntue <narunart.t@arise.tech>
2024-07-14 19:10:59 +0700

    [Sky] add test e2e get and delete

--- backend/go.mod
+++ backend/go.mod
@@ -5,7 +5,6 @@ go 1.22.4
 require (
 	github.com/gin-gonic/gin v1.10.0
 	github.com/lib/pq v1.10.9
-	github.com/mattn/go-sqlite3 v1.14.22
 	github.com/stretchr/testify v1.9.0
 	modernc.org/sqlite v1.30.1
 )
--- e2e/api/todo.spec.ts
+++ e2e/api/todo.spec.ts
@@ -1,46 +1,91 @@
-import { test, expect } from '@playwright/test'
+import { test, expect } from "@playwright/test";
 
-test('should response todo when post api/v1/todos', async ({
-    request,
+test("should response todo when post api/v1/todos", async ({ request }) => {
+  const data = {
+    title: "Test Todo",
+    status: "Active",
+  };
+  const reps = await request.post("api/v1/todos", { data });
+  const postJson = await reps.json();
+
+  expect(reps.ok()).toBeTruthy();
+  expect(await reps.json()).toEqual(
+    expect.objectContaining({
+      id: expect.any(Number),
+      title: data.title,
+      status: data.status,
+    })
+  );
+
+  const deleteResponse = await request.delete(`/api/v1/todos/${postJson.id}`);
+  expect(deleteResponse.ok()).toBeTruthy();
+});
+
+test("should response todo when get api/v1/todos/:id", async ({ request }) => {
+  const postData = {
+    title: "Test Todo",
+    status: "Active",
+  };
+  const repsPost = await request.post("api/v1/todos", { data: postData });
+  const postJson = await repsPost.json();
+
+  const reps = await request.get("api/v1/todos/" + postJson.id);
+
+  expect(reps.ok()).toBeTruthy();
+  expect(await reps.json()).toEqual(
+    expect.objectContaining({
+      id: expect.any(Number),
+      title: postData.title,
+      status: postData.status,
+    })
+  );
+
+  const deleteResponse = await request.delete(`/api/v1/todos/${postJson.id}`);
+  expect(deleteResponse.ok()).toBeTruthy();
+});
+
+test("should response todo when put api/v1/todos/:id", async ({ request }) => {
+  const postData = {
+    title: "Test Todo",
+    status: "Active",
+  };
+  const repsPost = await request.post("api/v1/todos", { data: postData });
+  const postJson = await repsPost.json();
+
+  const putData = {
+    title: "Test Update Todo",
+    status: "Inactive",
+  };
+  const reps = await request.put("api/v1/todos/" + postJson.id, {
+    data: putData,
+  });
+
+  expect(reps.ok()).toBeTruthy();
+  expect(await reps.json()).toEqual(
+    expect.objectContaining({
+      id: expect.any(Number),
+      title: putData.title,
+      status: putData.status,
+    })
+  );
+
+  const deleteResponse = await request.delete(`/api/v1/todos/${postJson.id}`);
+  expect(deleteResponse.ok()).toBeTruthy();
+});
+
+test("should response todo when delete api/v1/todos/:id", async ({
+  request,
 }) => {
-    const data = {
-        title: "Test Todo",
-        status: "Active"
-    }
-    const reps = await request.post('api/v1/todos', { data })
+  const data = {
+    title: "Test Todo",
+    status: "Active",
+  };
+  const reps = await request.post("api/v1/todos", { data });
+  const postJson = await reps.json();
 
-    expect(reps.ok()).toBeTruthy()
-    expect(await reps.json()).toEqual(
-        expect.objectContaining({
-            id: expect.any(Number),
-            title: data.title,
-            status: data.status
-        })
-    )
-})
+  const deleteResponse = await request.delete(`/api/v1/todos/${postJson.id}`);
 
-test('should response todo when put api/v1/todos', async ({
-    request,
-}) => {
-    const postData = {
-        title: "Test Todo",
-        status: "Active"
-    }
-    const repsPost = await request.post('api/v1/todos', { data: postData })
-    const postJson = await repsPost.json()
-
-    const putData = {
-        title: "Test Update Todo",
-        status: "Inactive"
-    }
-    const reps = await request.put('api/v1/todos/' + postJson.id, { data: putData })
-
-    expect(reps.ok()).toBeTruthy()
-    expect(await reps.json()).toEqual(
-        expect.objectContaining({
-            id: expect.any(Number),
-            title: putData.title,
-            status: putData.status
-        })
-    )
-})
+  expect(deleteResponse.ok()).toBeTruthy();
+  const deleteResult = await deleteResponse.json();
+  expect(deleteResult).toEqual("succes");
+});
